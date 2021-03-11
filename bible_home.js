4b984fbc64cc26441930f98e6e600e05784919f7
Caroline Hernandez <caroline.gillyweed@gmail.com>
2021-03-10 23:00:53 -0600

    ch

--- bible_home.js
+++ bible_home.js
@@ -42,22 +42,34 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
       
 
-     
+      document.querySelector('form').addEventListener('submit', async function(event){
   
-      let API_KEY = '17ce03c143dcb65dabd67475d0d56161c08e42e5'
-      let API_URL = 'https://api.esv.org/v3/passage/text/?q=John+3:16'
-      // let params = {
-      //   q: 'John+11:35'
-      // }
-      let headers = {
-        Authorization: `Token ${API_KEY}`
-      }
-      let response = await fetch(API_URL, {
-        headers: headers
+        event.preventDefault()
+
+        let verseRequested = document.querySelector('#passage-lookup').value
+        console.log(verseRequested)
+
+        let API_KEY = '17ce03c143dcb65dabd67475d0d56161c08e42e5'
+        let API_URL = `https://api.esv.org/v3/passage/text/?q=${verseRequested}`
+        let headers = {
+          Authorization: `Token ${API_KEY}`
+        }
+        let response = await fetch(API_URL, {
+          headers: headers
+        })
+        
+        let verseOutput = await response.json()
+        console.log(verseOutput)
+        
+        //let passages = verseOutput.passages
+
+        for (let i=0; i<verseOutput.passages.length;i++) {
+          document.querySelector('.passage').insertAdjacentHTML('beforeend', `
+            <div class="font-bold text-xl">${verseOutput.passage_meta[i].canonical}</div>
+            <div class="">${verseOutput.passages[i]}</div>
+          `)
+        }
       })
-      let verseOutput = await response.json()
-      console.log(verseOutput)  
-      
     
 
     } else {
--- prayer.js
+++ prayer.js
@@ -100,6 +100,9 @@ firebase.auth().onAuthStateChanged(async function(user) {
     }
 })
 
+//the two functions below are IDENTICAL, EXCEPT one writes prayers "before end", the other "after begin"
+//BOTH need the date (which is funneled in as "created") to be fixed. it's coming in from firebase as a number for seconds and nanoseconds; previously the "toDate" thing I commented worked but it stopped and IDK why 
+
 async function renderPrayer(userId, prayerId, username, title, description, completed, created) {
   //    let date = created.toDate().toDateString()
       document.querySelector('.render-prayers').insertAdjacentHTML('beforeend', `
